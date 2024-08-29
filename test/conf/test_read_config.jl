913bf436e18d1e15f20c7b0ecd90b37bde7f2ee0
martin <ga63loc@mytum.de>
2024-08-29 16:31:41 +0200

    added test for no spacing

--- test/conf/test_read_config.jl
+++ test/conf/test_read_config.jl
@@ -5,4 +5,7 @@ conf = get_config(filename=conf_path)
     @test conf("run_def") == "true"
     @test conf("n", "Ga") == "3"
     @test conf("orbitals", "Ga") == "sp3dr2 sp3dr2 sp3dr2 sp3dr2"
+
+    # test sp_geo=true without spaces
+    @test conf("sp_geo") == "true"
 end
