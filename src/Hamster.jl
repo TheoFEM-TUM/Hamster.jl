548b1ddb623b17a440ea6cdb087a152aff5defc1
martin <ga63loc@mytum.de>
2024-08-30 12:19:05 +0200

    updated includes and exports

--- src/Hamster.jl
+++ src/Hamster.jl
@@ -5,10 +5,13 @@ using TensorOperations, LinearAlgebra, SparseArrays, KrylovKit
 include("parse/utils.jl")
 include("conf/config.jl"); include("conf/read_config.jl")
 
-include("model/hamiltonian.jl")
+include("model/ham.jl"); include("model/ham_write.jl")
+
+export write_to_file, read_from_file
 
 export Config, get_config
 
 export get_hamiltonian, diagonalize
+export write_hr, read_hr
 
 end # module Hamster
