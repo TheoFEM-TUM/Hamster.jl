bd98be57b5f360e9d6b0d49f7dbb8c975d2070f9
martin <ga63loc@mytum.de>
2024-09-03 16:23:54 +0200

    updated includes

--- src/Hamster.jl
+++ src/Hamster.jl
@@ -7,13 +7,15 @@ include("parse/utils.jl"); include("parse/poscar.jl")
 include("conf/config.jl"); include("conf/read_config.jl")
 include("conf/strc_defaults.jl")
 
-include("strc/vec.jl"); include("strc/grid.jl"); #include("strc/lattice.jl")
+include("strc/vec.jl"); include("strc/grid.jl"); include("strc/lattice.jl")
 
 include("model/ham.jl"); include("model/ham_write.jl")
 
 export write_to_file, read_from_file
 
 export Config, get_config
+
+export findR0
 
 export get_hamiltonian, diagonalize
 export write_hr, read_hr
