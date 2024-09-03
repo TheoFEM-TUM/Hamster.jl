d3c50e8aab2434341f922f5ec447c202eb300836
martin <ga63loc@mytum.de>
2024-09-03 12:15:10 +0200

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,19 @@
-using Hamster, Test, LinearAlgebra, SparseArrays
+using Hamster, Test, LinearAlgebra, SparseArrays, StaticArrays
 
+@testset "Parse" begin
+    include("parse/test_utils.jl")
+    include("parse/test_poscar.jl")
+end
 
-include("conf/test_config.jl")
-include("conf/test_read_config.jl")
-include("parse/test_utils.jl")
+@testset "Config" begin
+    include("conf/test_config.jl")
+    include("conf/test_read_config.jl")
+end
+
+@testset "Structure" begin
+    include("strc/test_vec.jl")
+    include("strc/test_grid.jl")
+end
 
 @testset "model" begin
     include("model/test_ham.jl")
