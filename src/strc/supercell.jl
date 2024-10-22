4408f790b6618ff92cb7a1308ee179e4694fdb99
martin <ga63loc@mytum.de>
2024-10-22 16:09:07 +0200

    moved method to get config indices

--- src/strc/supercell.jl
+++ src/strc/supercell.jl
@@ -46,3 +46,27 @@ function get_structures(conf=get_empty_config(); mode="md", index_file="config_i
         return [Structure(conf)], [1]
     end
 end
+
+"""
+    get_config_index_sample(Nconf_max, conf=get_empty_config(); Nconf=get_Nconf(conf), Nconf_min=get_Nconf_min(conf), val_ratio=get_val_ratio(conf))
+
+Randomly selects training and validation configuration indices from a given range of configurations.
+
+# Arguments
+- `Nconf_max`: The maximum configuration index.
+- `conf`: (Optional) A configuration object from which additional parameters are obtained. Defaults to an empty configuration.
+- `Nconf`: (Optional) The number of configurations to sample for training, fetched from the configuration object if not provided.
+- `Nconf_min`: (Optional) The minimum configuration index, fetched from the configuration object if not provided.
+- `val_ratio`: (Optional) The ratio of validation data size to training data size, fetched from the configuration object if not provided.
+
+# Returns
+- `train_config_inds`: A vector of indices for training configurations.
+- `val_config_inds`: A vector of indices for validation configurations.
+"""
+function get_config_index_sample(Nconf_max, conf=get_empty_config(); Nconf=get_Nconf(conf), Nconf_min=get_Nconf_min(conf), val_ratio=get_val_ratio(conf))
+    Nval = round(Int64, Nconf * val_ratio)
+    train_config_inds = sample(Nconf_min:Nconf_max, Nconf, replace=false, ordered=true)
+    remaining_indices = setdiff(Nconf_min:Nconf_max, train_config_inds)
+    val_config_inds = sample(remaining_indices, Nval, replace=false, ordered=true)
+    return train_config_inds, val_config_inds
+end
--- test/strc/test_supercell.jl
+++ test/strc/test_supercell.jl
@@ -31,3 +31,26 @@ path = string(@__DIR__) * "/test_files/"
     end
     @test all(correct_atom_positions)
 end
+
+@testset "Test Config indices" begin
+    # Test 1: no validation, no Nconf_min
+    conf = get_empty_config()
+    set_value!(conf, "Nconf", "Supercell", 100)
+    set_value!(conf, "Nconf_min", "Supercell", 1)
+    config_indices, _ = Hamster.get_config_index_sample(1000, conf)
+    @test length(unique(config_indices)) == 100
+    @test all(i->1 ≤ i ≤ 1000, config_indices)
+
+    # Test 2: no validation, Nconf_min set to 100, no Config
+    config_indices, _ = Hamster.get_config_index_sample(1000, Nconf=100, Nconf_min=100)
+    @test length(unique(config_indices)) == 100
+    @test all(i->100 ≤ i ≤ 1000, config_indices)
+
+    # Test 3: validation with ratio 0.2, Nconf_min set to 100
+    train_indices, val_indices = Hamster.get_config_index_sample(1000, Nconf=100, Nconf_min=75, val_ratio=0.2)
+    @test length(unique(train_indices)) == 100
+    @test length(unique(val_indices)) == 20
+    @test all(i->75 ≤ i ≤ 1000, train_indices)
+    @test all(i->75 ≤ i ≤ 1000, val_indices)
+    @test isempty(intersect(train_indices, val_indices))
+end
