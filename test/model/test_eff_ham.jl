f95a3c4d0611b59ec100dae685278f7568a03ffc
martin <ga63loc@mytum.de>
2024-11-14 17:02:34 +0100

    cleanup

--- test/model/test_eff_ham.jl
+++ test/model/test_eff_ham.jl
@@ -7,19 +7,5 @@ path = string(@__DIR__) * "/test_files/"
     eff_ham = EffectiveHamiltonian(conf)
     get_hr(eff_ham, 1)
     #@code_warntype get_hr(eff_ham, 1)
-    @code_warnget_hr(h::AbstractMatrix, V::AbstractVector, mode=Val{:dense}; apply_soc=false)
     rm("rllm.dat")
 end
-
-conf = get_empty_config()
-set_value!(conf, "poscar", joinpath(path, "POSCAR_gaas"))
-set_value!(conf, "save_rllm", false)
-eff_ham = EffectiveHamiltonian(conf)
-get_hr(eff_ham, 1)
-@code_warntype get_hr(eff_ham, 1)
-
-@show typeof(eff_ham)
-@code_warntype get_hr(eff_ham.models[1].hs[1], eff_ham.models[1].V, Val{:dense}, apply_soc=false)
-rm("rllm.dat")
-
-@show Hamster.get_sp_mode(conf) :: Type{Hamster.get_sp_mode(conf)} 
