19b3098d9c1209a81fee4b035dcf2c51dd086580
martin <ga63loc@mytum.de>
2024-11-27 23:30:00 +0100

    fixed hardcoded number of bands

--- src/calc/optimization.jl
+++ src/calc/optimization.jl
@@ -44,7 +44,7 @@ function run_calculation(::Val{:optimization}, comm, conf::Config; rank=0, nrank
    val_bases = Basis[Basis(strc, conf) for strc in val_strcs]
    ham_val = EffectiveHamiltonian(val_strcs, val_bases, conf)
 
-   dl = DataLoader(local_train_inds, local_val_inds, 8, 8, conf)
+   dl = DataLoader(local_train_inds, local_val_inds, length(train_bases[1]), length(train_bases[end]), conf)
    Nε, Nk = get_neig_and_nk(dl.train_data)
    optim = GDOptimizer(Nε, Nk, conf)
    prof = HamsterProfiler(3, conf)
