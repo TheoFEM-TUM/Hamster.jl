9a44518d2e7cee0d1a05f842be8330c4dc78843c
martin <ga63loc@mytum.de>
2025-02-24 12:52:15 +0100

    improved test coverage

--- test/calc/test_optimization.jl
+++ test/calc/test_optimization.jl
@@ -27,6 +27,22 @@ end
     rm("hamster.out"); rm("train_config_inds.dat"); rm("val_config_inds.dat"); rm("params.dat"); rm("ml_params.dat")
 end
 
+@testset "Optimization workflow for ML+TB (sparse)" begin
+    path = joinpath(@__DIR__, "test_files")
+    conf = get_config(filename = joinpath(path, "hconf_ml"))
+    set_value!(conf, "poscar", joinpath(path, "POSCAR_gaas"))
+    set_value!(conf, "rllm_file", joinpath(path, "rllm.dat"))
+    set_value!(conf, "sp_mode", true)
+    set_value!(conf, "train_data", "Optimizer", joinpath(path, "EIGENVAL_gaas"))
+    set_value!(conf, "val_data", "Optimizer", joinpath(path, "EIGENVAL_gaas"))
+    set_value!(conf, "init_params", joinpath(path, "params.dat"))
+
+    prof = Hamster.main(comm, conf, rank=rank)
+    @test mean(prof.L_train[:, end]) < 0.1
+    @test prof.L_val[end] < 0.3 # includes all bands
+    rm("hamster.out"); rm("train_config_inds.dat"); rm("val_config_inds.dat"); rm("params.dat"); rm("ml_params.dat")
+end
+
 @testset "Optimization for MD data" begin
     path = joinpath(@__DIR__, "test_files")
     conf = get_config(filename = joinpath(path, "hconf_md"))
