0dc5ff83ac8b01caa5cedb475b52ef693d37f6e8
martin <ga63loc@mytum.de>
2025-02-26 21:47:01 +0100

    moved nbatch tag to supercell block

--- src/conf/supercell_defaults.jl
+++ src/conf/supercell_defaults.jl
@@ -20,6 +20,13 @@ The `Nconf_max` tag sets the maximum index that can be sampled from the total nu
 get_Nconf_max(conf::Config)::Int64 = get(conf, "Nconf_max", "Supercell", 1)
 
 """
+    nbatch=1
+
+The `nbatch` tag detemines into how many batches the training structures are split for stochastic gradient optimization.
+"""
+get_nbatch(conf::Config)::Int64 = conf("nbatch", "Supercell") == "default" ? 1 : conf("nbatch", "Supercell")
+
+"""
     sc_poscar=SC_POSCAR
 
 The `sc_poscar` tag sets the path to the POSCAR file used for the supercell.
