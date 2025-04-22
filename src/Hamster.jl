a54b765d52e16ef170b088682fafa2689db6232e
martin <ga63loc@mytum.de>
2025-04-22 11:15:13 +0200

    removed Distributions from deps

--- src/Hamster.jl
+++ src/Hamster.jl
@@ -1,7 +1,7 @@
 module Hamster
 
 using LinearAlgebra, SparseArrays, StaticArrays, KrylovKit, Dates, PeriodicTable, UnPack, MPI, Clustering, BlockDiagonals,
-    CubicSplines, HCubature, Statistics, ChunkSplitters, FiniteDiff, StatsBase, HDF5, Printf, OhMyThreads, Distributions
+    CubicSplines, HCubature, Statistics, ChunkSplitters, FiniteDiff, StatsBase, HDF5, Printf, OhMyThreads
 
 include("parse/utils.jl"); include("parse/poscar.jl"); include("parse/eigenval.jl"); include("parse/xdatcar.jl"); include("parse/wannier90.jl")
 include("parse/commandline.jl")
