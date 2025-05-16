568392e265c24ad08a0cb9fde08c02561e7a1948
martin <ga63loc@mytum.de>
2025-05-16 10:29:00 +0200

    adapted hyperparameter optimization to allow for tpe

--- test/calc/test_hyperopt.jl
+++ test/calc/test_hyperopt.jl
@@ -8,7 +8,7 @@
     @test std(prof.L_train) > 0
     @test std(prof.timings) > 0
     rm("hamster.out"); rm("train_config_inds.dat"); rm("val_config_inds.dat"); rm("params.dat")
-    rm("L_val.dat"); rm("L_train.dat"); rm("rllm.dat"); rm("hyperopt_out.h5")
+    rm("L_val.dat"); rm("L_train.dat"); rm("rllm.dat"); rm("hamster_out.h5")
 end
 
 @testset "Grid search hyperparameter optimization" begin
@@ -28,5 +28,19 @@ end
     @test std(prof.timings) > 0
     @test h5read("hyperopt_out.h5", "param_values") == [5.0 6.0 7.0]
     rm("hamster.out"); rm("train_config_inds.dat"); rm("val_config_inds.dat"); rm("params.dat")
-    rm("L_val.dat"); rm("L_train.dat"); rm("rllm.dat"); rm("hyperopt_out.h5")
+    rm("L_val.dat"); rm("L_train.dat"); rm("rllm.dat"); rm("hamster_out.h5")
+end
+
+@testset "TPE hyperparameter optimization" begin
+    path = joinpath(@__DIR__, "test_files")
+    conf = get_config(filename = joinpath(path, "hconf_hyperopt"))
+    set_value!(conf, "poscar", joinpath(path, "POSCAR_gaas"))
+    set_value!(conf, "train_data", "Optimizer", joinpath(path, "EIGENVAL_gaas"))
+    set_value!(conf, "mode", "HyperOpt", "tpe")
+
+    prof = Hamster.main(comm, conf, rank=rank)
+    @test std(prof.L_train) > 0
+    @test std(prof.timings) > 0
+    rm("hamster.out"); rm("train_config_inds.dat"); rm("val_config_inds.dat"); rm("params.dat")
+    rm("L_val.dat"); rm("L_train.dat"); rm("rllm.dat"); rm("hamster_out.h5")
 end
