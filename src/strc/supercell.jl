b8ba983f2936d1bca28936e58a2d488279271dbc
martin <ga63loc@mytum.de>
2025-06-17 10:50:56 +0200

    lattice is 3rd rank tensor

--- src/strc/supercell.jl
+++ src/strc/supercell.jl
@@ -17,7 +17,7 @@ function get_structures(conf=get_empty_config(); Rs=zeros(3, 1), mode="pc", conf
     if lowercase(mode) == "md" || lowercase(mode) == "mixed"
         sc_poscar = read_poscar(sc_poscar)
         @unpack rs_atom, atom_types = sc_poscar
-        lattice, configs = occursin(".h5", xdatcar) ? (h5read(xdatcar, "lattice"), h5read(xdatcar, "positions")) : read_xdatcar(xdatcar, frac=false)
+        lattice, configs = occursin(".h5", xdatcar) ? (h5read(xdatcar, "lattice")[:, :, 1], h5read(xdatcar, "positions")) : read_xdatcar(xdatcar, frac=false)
         
         # Check that POSCAR lattice and XDATCAR lattice are compatible
         @assert sc_poscar.lattice ≈ lattice
