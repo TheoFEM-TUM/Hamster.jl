9ee97402fb49535a7b4809257cd7168f5498a0d5
martin <ga63loc@mytum.de>
2025-06-25 09:27:18 +0200

    verbosity is now a function argument of main

--- src/main.jl
+++ src/main.jl
@@ -1,4 +1,4 @@
-function main(comm, conf; rank=0, nranks=1, num_nodes=1)
+function main(comm, conf; rank=0, nranks=1, num_nodes=1, verbosity=get_verbosity(conf))
     hostnames = MPI.gather(readchomp(`hostname`), comm, root=0)
     if rank == 0
         generate_output(conf, hostnames=hostnames)
