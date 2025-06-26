5f0caecda3973517b142a38387fd2894b0e7c46c
martin <ga63loc@mytum.de>
2025-06-26 12:10:32 +0200

    changed logic in if statement

--- src/strc/supercell.jl
+++ src/strc/supercell.jl
@@ -20,7 +20,7 @@ function get_structures(conf=get_empty_config(); Rs=zeros(3, 1), mode="pc", conf
 
         if occursin(".h5", xdatcar)
             h5open(xdatcar, "r") do file
-                pos_key = haskey(file, "positions") ? "positions" : "configs"
+                pos_key = haskey(file, "configs") ? "configs" : "positions"
             end
             lattice, configs = (h5read(xdatcar, "lattice")[:, :, 1], h5read(xdatcar, "configs"))
             for n in axes(configs, 3)
