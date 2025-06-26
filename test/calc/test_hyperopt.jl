73f854e1aa9105885dede58a48c89746f0de244b
martin <ga63loc@mytum.de>
2025-06-26 09:45:09 +0200

    reduced calc test workload, added test for optimization of ML+SOC in sparse mode

--- test/calc/test_hyperopt.jl
+++ test/calc/test_hyperopt.jl
@@ -16,7 +16,7 @@ end
     set_value!(conf, "poscar", joinpath(path, "POSCAR_gaas"))
     set_value!(conf, "train_data", "Optimizer", joinpath(path, "EIGENVAL_gaas"))
     set_value!(conf, "lowerbounds", "HyperOpt", 5)
-    set_value!(conf, "upperbounds", "HyperOpt", 7)
+    set_value!(conf, "upperbounds", "HyperOpt", 6)
     set_value!(conf, "params", "HyperOpt", "rcut")
     set_value!(conf, "stepsizes", "HyperOpt", "1")
     set_value!(conf, "mode", "HyperOpt", "grid")
@@ -25,7 +25,7 @@ end
     prof = Hamster.main(comm, conf, rank=rank)
     @test std(prof.L_train) > 0
     @test std(prof.timings) > 0
-    @test h5read("hamster_out.h5", "rcut") == [5.0, 6.0, 7.0]
+    @test h5read("hamster_out.h5", "rcut") == [5.0, 6.0]
     rm("hamster.out"); rm("rllm.dat"); rm("hamster_out.h5")
 end
 
