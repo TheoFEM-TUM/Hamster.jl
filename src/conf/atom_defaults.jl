832d340bf0fa6f0e60c4a9934a038d9b86bc3f36
martin <ga63loc@mytum.de>
2025-09-02 13:19:31 +0200

    fixed docstrings

--- src/conf/atom_defaults.jl
+++ src/conf/atom_defaults.jl
@@ -1,20 +1,20 @@
 """
-    alpha=0.7*Z
+**alpha**=0.7*Z
 
-The `alpha` value determines how rapid the orbital overlap for a specific ion type falls off with distance. Defaults to 70% of the core charge.
+The `alpha::Float` value determines how rapid the orbital overlap for a specific ion type falls off with distance. Defaults to 70% of the core charge.
 """
 get_alpha(conf::Config, type)::Float64 = conf("alpha", type) == "default" ? 0.70 * elements[Symbol(type)].number : conf("alpha", type)
 
 """
-    n=n_period
+**n**=n_period
 
-The `n` value determines the order of the polynomial that is used to model the distance dependence the orbital overlap for a specific ion type. Defaults to the period the atom species belongs to.
+The `n::Int` value determines the order of the polynomial that is used to model the distance dependence the orbital overlap for a specific ion type. Defaults to the period the atom species belongs to.
 """
 get_n(conf::Config, type)::Int64 = conf("n", type) == "default" ? elements[Symbol(type)].period : conf("n", type)
 
 """
-    NNaxes=false
+**nnaxes**=false
 
-If `NNaxes=true` the orbital axes are rotated along the connecting vectors with the nearest neighbors of the respective orbitals. The number of nearest neighbors depends on the number of orbitals.
+If `nnaxes::Bool=true`, the orbital axes are rotated along the connecting vectors with the nearest neighbors of the respective orbitals. The number of nearest neighbors depends on the number of orbitals.
 """
-get_nnaxes(conf::Config, type)::Bool = conf("NNaxes", type) == "default" ? false : conf("NNaxes", type)
+get_nnaxes(conf::Config, type)::Bool = conf("nnaxes", type) == "default" ? false : conf("nnaxes", type)
