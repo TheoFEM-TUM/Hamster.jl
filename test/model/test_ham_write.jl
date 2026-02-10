07818d995b8ea2efd7840d8acca07c49f2f647e8
martin <ga63loc@mytum.de>
2026-02-10 15:45:29 +0100

    added tests for current writing, expanded hamiltonian tests

--- test/model/test_ham_write.jl
+++ test/model/test_ham_write.jl
@@ -1,6 +1,6 @@
 @testset "Hamiltonian I/O" begin
     H_original = [rand(5,5), sprand(5,5,0.3)]  # mix of dense and sparse
-    vecs_original = rand(3, 5)
+    vecs_original = rand(3, 2)
     
     write_ham(H_original, vecs_original, comm, rank; space="k")
     H_loaded, vecs_loaded = read_ham(comm, rank; space="k")
@@ -20,8 +20,50 @@
         h_sparse = issparse(h_orig) ? h_orig : sparse(h_orig)
         @test h_sparse ≈ h_loaded
     end
+
+    rm("ham.h5", force=true)
+
+    # Test for more R - vectors
+    NR = 12
+    Hrs = [rand(5, 5) for R in 1:NR]
+    Rs = rand(3, NR)
+    write_ham(Hrs, Rs, comm, rank; space="r")
+    Hrs_loaded, Rs_loaded = read_ham(comm, rank; space="r")
+    @test Hrs == Hrs_loaded
+    @test Rs == Rs_loaded
+
+    rm("ham.h5", force=true)
 end
 
-if rank == 0
+
+@testset "Current I/O" begin
+    ħ_eVfs = 0.6582119569
+    filepath = joinpath(@__DIR__, "test_files")
+    conf = get_config(filename=joinpath(filepath, "hconf_gaas"))
+    set_value!(conf, "poscar", joinpath(filepath, "POSCAR_gaas"))
+    set_value!(conf, "rllm_file", joinpath(filepath, "rllm_true.dat"))
+    set_value!(conf, "verbosity", 0)
+    
+    strc = Structure(conf)
+    basis = Basis(strc, conf)
+    model = TBModel(strc, basis, conf)
+
+    Hr = get_hr(model, Hamster.Dense())
+    write_ham(Hr, strc.Rs, comm, rank; space="r")
+
+    bonds = Hamster.get_bonds(strc, basis, conf)
+    current_true = [-1im/ħ_eVfs .* bonds[R] .* Hr[R] for R in eachindex(Hr)]
+
+    Hamster.write_current(bonds, comm, 0; filename="ham.h5", system="", rank=0, nranks=1)
+    current_trial, vecs = Hamster.read_current(rank)
+
+    @test vecs == strc.Rs
+
+    correct_current = Bool[]
+    for R in eachindex(current_true), j in axes(current_true[R], 2), i in axes(current_true[R], 1)
+        push!(correct_current, norm(current_trial[R][i, j] .- current_true[R][i, j]) ≈ 0)
+    end
+    @test all(correct_current)
+
     rm("ham.h5", force=true)
 end
