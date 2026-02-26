fca8a8c865dc5e1b2f9260304560aff7eba1d01e
martin <ga63loc@mytum.de>
2026-02-26 18:01:07 +0100

    cosmetics

--- src/main.jl
+++ src/main.jl
@@ -26,9 +26,7 @@ function main(comm, conf; rank=0, nranks=1, num_nodes=1, verbosity=get_verbosity
             nstrc_per_hamster=round(Int64, Nconf/nranks), nstrc_per_node=round(Int64, Nconf/num_nodes), 
             julia_num_threads=julia_num_threads, nthreads_kpoints=nthreads_kpoints, 
             nthreads_bands=nthreads_bands, nthreads_blas=nthreads_blas)
-    end
     
-    if rank == 0
         blocks_to_write = string.(vcat(["Options"], keys(conf.blocks)...))
         filter!(block -> block ∈ unique([tag.block for tag in CONFIG_TAGS]), blocks_to_write)
         write_config_tags(conf; blocks=blocks_to_write, show_desc=true)
